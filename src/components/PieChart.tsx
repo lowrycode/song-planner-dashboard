34f5333a9b77b209cd114879467ea1b2d99b7c47
Pete Lowry <peter.m.lowry@gmail.com>
2025-12-11 12:50:03 +0000

    Add pie charts to show proportion of hymns and song keys on the overview page

---
+++ src/components/PieChart.tsx
@@ -0,0 +1,74 @@
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const basePalette = [
+  "#ef4444",
+  "#3b82f6",
+  "#fbbf24",
+  "#10b981",
+  "#8b5cf6",
+  "#f472b6",
+  "#f97316",
+  "#6366f1",
+  "#14b8a6",
+  "#eab308",
+  "#22c55e",
+  "#ec4899",
+];
+
+type PieChartProps = {
+  data: number[];
+  labels: string[];
+  colors?: string[];
+  title?: string;
+};
+
+function PieChart({
+  data,
+  labels,
+  colors = [],
+  title = "",
+}: PieChartProps) {
+  const bgColors =
+    colors.length > 0
+      ? colors
+      : data.map((_, i) => basePalette[i % basePalette.length]);
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        data,
+        backgroundColor: bgColors,
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: "right" as const, // default is 'top', change it to 'right'
+        labels: {
+          // Optional: customize label styles here
+          boxWidth: 20,
+          padding: 10,
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="bg-gray-100 p-4 rounded-lg">
+      <h2 className="text-xl font-extrabold text-purple-900 mb-5">{title}</h2>
+      <div className="flex flex-1">
+        <Doughnut data={chartData} options={options} />
+      </div>
+    </div>
+  );
+}
+
+export default PieChart
--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -1,6 +1,10 @@
 import TableSortSearch from "../components/TableSortSearch.tsx";
+import PieChart from "../components/PieChart.tsx";
 import songs from "../../sample_data/song_list.json";
+import hymn_pie from "../../sample_data/hymn_pie.json";
+import key_pie from "../../sample_data/key_pie.json";
 
+// Songs Table
 const songs_processed = songs.map((song) => ({
   id: song.id,
   first_line: song.first_line,
@@ -8,24 +12,29 @@ const songs_processed = songs.map((song) => ({
   total: song.total,
 }));
 
-
 const headerMap = {
-  "first_line": "Song (First Line)",
-  "newland": "Newland",
-  "riverside": "Riverside",
-  "hessle": "Hessle",
-  "orchard_park": "Orchard Park",
-  "network": "Network",
-  "total": "Total",
+  first_line: "Song (First Line)",
+  newland: "Newland",
+  riverside: "Riverside",
+  hessle: "Hessle",
+  orchard_park: "Orchard Park",
+  network: "Network",
+  total: "Total",
 };
 
-const searchKeys = ["first_line"]
+const searchKeys = ["first_line"];
+
+// Pie Charts
+const hymnPieLabels = hymn_pie.map(obj => Object.keys(obj)[0]);
+const hymnPieData   = hymn_pie.map(obj => Object.values(obj)[0]);
+const keyPieLabels = key_pie.map(obj => Object.keys(obj)[0]);
+const keyPieData   = key_pie.map(obj => Object.values(obj)[0]);
 
 function OverviewPage() {
   return (
     <div>
-      <div className="flex h-9/10 m-5">
-        <div className="w-full">
+      <div className="flex flex-wrap gap-5 m-5">
+        <div className="flex-3">
           <TableSortSearch
             headerMap={headerMap}
             data={songs_processed}
@@ -33,6 +42,14 @@ function OverviewPage() {
             searchPlaceholder="Filter by song"
             title="Song Counts"
           />
+        </div>
+        <div className="flex flex-1 justify-between flex-wrap gap-5">
+            <PieChart
+              data={hymnPieData}
+              labels={hymnPieLabels}
+              title="Hymn vs Song"
+            />
+            <PieChart data={keyPieData} labels={keyPieLabels} title="Key" />
         </div>
       </div>
     </div>
