443a1e17af5c9686b9c5c39f8abc9fa33a30e785
Pete Lowry <peter.m.lowry@gmail.com>
2025-12-22 15:57:16 +0000

    Fix typo: 'wush' to 'wish'

--- src/components/LogoutForm.tsx
+++ src/components/LogoutForm.tsx
@@ -43,7 +43,7 @@ function LogoutForm() {
         Logout
       </h1>
       <p className="text-gray-600 text-center text-sm mb-8">
-        Are you sure you wush to logout?
+        Are you sure you wish to logout?
       </p>
       <button onClick={handleLogout} className="bg-purple-900 text-gray-100 px-6 py-2 rounded-md text-center hover:bg-purple-700 hover:cursor-pointer">
         Logout
