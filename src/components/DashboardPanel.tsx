481e4651740908c1952fd9452f0078d1d97addf0
Pete Lowry <peter.m.lowry@gmail.com>
2025-12-23 21:46:19 +0000

    Adjust padding to improve spacing inside component

--- src/components/DashboardPanel.tsx
+++ src/components/DashboardPanel.tsx
@@ -11,7 +11,7 @@ export default function DashboardPanel({
 }: DashboardPanelProps) {
   return (
     <div
-      className={`bg-gray-100 p-4 rounded-lg shadow-sm ${className}`}
+      className={`bg-gray-100 px-5 py-3 rounded-lg shadow-sm ${className}`}
     >
       {children}
     </div>
