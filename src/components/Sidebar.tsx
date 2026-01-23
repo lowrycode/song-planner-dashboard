9400fc6b99666cf42b33c1ae92fc2600876b61e6
Pete Lowry <lowrysoftdev@gmail.com>
2026-01-23 22:35:48 +0000

    Hide Admin pages from non-admin users in the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,8 +4,15 @@ import { BsCircleHalf } from "react-icons/bs";
 import { FaUnlock } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import SidebarSection from "./SidebarSection";
+import { useAuth } from "../hooks/useAuth";
+
 
 export default function Sidebar() {
+  const { user } = useAuth();
+  const isAdmin = (user?.role === "admin");
+
+  console.log(user);
+
   const dashboardLinks = [
     { to: "/overview", icon: <MdSpaceDashboard />, label: "Overview" },
     { to: "/search", icon: <FaMagnifyingGlass />, label: "Search" },
@@ -22,13 +29,16 @@ export default function Sidebar() {
   ];
 
   return (
-    <nav className="w-48 bg-gray-900 text-gray-200 p-4" aria-label="Main navigation">
+    <nav
+      className="w-48 bg-gray-900 text-gray-200 p-4"
+      aria-label="Main navigation"
+    >
       <header className="flex items-center justify-center">
         <img src="/images/ccnetwork_logo.png" alt="CCN logo" />
       </header>
 
       <SidebarSection title="DASHBOARD" links={dashboardLinks} />
-      <SidebarSection title="ADMIN" links={adminLinks} />
+      {isAdmin && <SidebarSection title="ADMIN" links={adminLinks} />}
       <SidebarSection title="ACCOUNT" links={accountLinks} />
     </nav>
   );
