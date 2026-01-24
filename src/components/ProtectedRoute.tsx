a17a52110dd7f786465fdb467faef5bc6d19d774
Pete Lowry <lowrysoftdev@gmail.com>
2026-01-24 13:28:14 +0000

    Improve auth loading UX and prevent redirect after login when previous page was the logout page

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,18 +1,28 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
+import Spinner from "./Spinner";
 
 export default function ProtectedRoute() {
   const location = useLocation();
   const { user, userLoading } = useAuth();
 
+  // Show placeholder until auth state is resolved
   if (userLoading) {
-    // Show placeholder until auth state is resolved
-    return <div>Getting auth state...</div>;
+    return (
+      <div className="flex h-screen flex-col items-center justify-center gap-3">
+        <div className="relative">
+          <Spinner message="Checking your session..." />
+        </div>
+      </div>
+    );
   }
 
+  // Not logged in, redirect to login
   if (!user) {
-    // Not logged in, redirect to login
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    // Remove logout page from redirect behaviour
+    const from = location.pathname === "/logout" ? undefined : location;
+
+    return <Navigate to="/login" state={from ? { from } : undefined} replace />;
   }
 
   return <Outlet />;
