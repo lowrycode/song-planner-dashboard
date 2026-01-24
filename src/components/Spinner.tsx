3b6922c8749c1783ea16d21d30538ab131081720
Pete Lowry <lowrysoftdev@gmail.com>
2026-01-24 12:46:24 +0000

    Update spinner to optionally allow passing a message as prop

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,13 +1,14 @@
 import { ClipLoader } from "react-spinners";
 
-export default function Spinner() {
+interface SpinnerProps {
+  message?: string;
+}
+
+export default function Spinner({ message = "Loading..." }: SpinnerProps) {
   return (
     <div className="flex flex-col flex-1 gap-3 justify-center items-center text-center">
-      <div>
       <ClipLoader color="#3b82f6" size={30} />
-      <div className="text-sm text-gray-500">Loading..</div>
-
-      </div>
+      <div className="text-sm text-gray-500">{message}</div>
     </div>
   );
 }
