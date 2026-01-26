c634b796cc082226fa4be0a1444734f33ab99d1f
Pete Lowry <lowrysoftdev@gmail.com>
2026-01-26 22:58:19 +0000

    Improve style of MobileTopBar
    - Enlarge hamburger
    - Reduce vertical padding

--- src/components/MobileTopBar.tsx
+++ src/components/MobileTopBar.tsx
@@ -7,7 +7,7 @@ interface MobileTopBarProps {
 
 function MobileTopBar({ setMobileOpen }: MobileTopBarProps) {
   return (
-    <div className="flex justify-between bg-gray-900 w-full md:hidden text-xl p-4">
+    <div className="flex justify-between bg-gray-900 w-full md:hidden text-2xl px-4 py-3">
       <img src="/images/ccnetwork_logo.png" alt="CCN logo" className="w-36" />
       <button
         className="text-gray-400 hover:text-white"
