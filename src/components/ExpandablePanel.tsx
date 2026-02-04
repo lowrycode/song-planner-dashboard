83338e0115d59923eba9128d3cc673af7a37f782
Pete Lowry <lowrysoftdev@gmail.com>
2026-02-04 22:36:06 +0000

    Make ExpandablePanel component more reusable with optional props and defaultExpanded
    - Optional props (title, caption, recordCount, defaultExpanded) allow for more control and reuse in different circumstances (non-tables)
    - `type=button` allows for use with forms and avoids accidental form submissions

--- src/components/ExpandablePanel.tsx
+++ src/components/ExpandablePanel.tsx
@@ -4,44 +4,55 @@ import { BsFillCaretDownFill } from "react-icons/bs";
 import { BsFillCaretUpFill } from "react-icons/bs";
 
 interface ExpandablePanelProps {
-  title: string;
-  recordCount: number;
+  title?: string;
+  caption?: ReactNode;
+  recordCount?: number;
   className?: string;
+  defaultExpanded?: boolean;
   children: ReactNode;
 }
 
 export default function ExpandablePanel({
   title,
+  caption,
   recordCount,
   className = "",
+  defaultExpanded = false,
   children,
 }: ExpandablePanelProps) {
-  const [expanded, setExpanded] = useState(false);
-  
+  const [expanded, setExpanded] = useState(defaultExpanded);
+
+  const shouldShowChildren =
+    expanded && (recordCount === undefined || recordCount > 0);
+
   return (
-    <div className={`bg-gray-100 px-5 py-3 rounded-lg shadow-sm w-full ${className}`}>
+    <div
+      className={`bg-gray-100 px-5 py-3 rounded-lg shadow-sm w-full ${className}`}
+    >
       <button
+        type="button"
         onClick={() => setExpanded((s) => !s)}
         className="w-full flex items-center justify-between hover:cursor-pointer"
         aria-expanded={expanded}
       >
         <div className="flex gap-2 items-center">
-          <h2 className="text-xl font-extrabold text-purple-900">{title}</h2>
-          <div className="text-sm text-gray-600">({recordCount})</div>
+          {title && (
+            <h2 className="text-xl font-extrabold text-purple-900">{title}</h2>
+          )}
+          {caption && <p className="text-purple-900 font-bold text-sm">{caption}</p>}
+          {recordCount !== undefined && (
+            <div className="text-sm text-gray-600">({recordCount})</div>
+          )}
         </div>
-        <div className="text-gray-500 ml-4">{expanded ? <BsFillCaretUpFill/> : <BsFillCaretDownFill/>}</div>
+        <div className="text-gray-500 ml-4">
+          {expanded ? <BsFillCaretUpFill /> : <BsFillCaretDownFill />}
+        </div>
       </button>
 
-      {expanded && recordCount > 0 && (
-        <>
-          {children}
-        </>
-      )}
+      {shouldShowChildren && <>{children}</>}
 
       {expanded && recordCount === 0 && (
-        <div className="mt-3 text-gray-600 text-sm">
-          No data to display.
-        </div>
+        <div className="mt-3 text-gray-600 text-sm">No data to display.</div>
       )}
     </div>
   );
